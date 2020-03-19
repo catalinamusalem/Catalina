94e8f08b9bb09b1858802054c425884905e5a53c
catalinamusalem <catalina.musalem@uc.cl>
2020-03-19 15:43:36 -0300

    cargar

--- Actividades/AF01/cargar.py
+++ Actividades/AF01/cargar.py
@@ -1,6 +1,7 @@
 from collections import namedtuple, deque
 
-
+animes= namedtuple("animes", ["nombre", "rating", "estudio", "genero"])
+diccionario1={}
 def cargar_animes(path):
     # Abrimos el archivo de animes
     with open(path, 'r', encoding="utf-8") as file:
@@ -10,16 +11,32 @@ def cargar_animes(path):
             anime = line.strip().split(",")
             # Separamos los generos por slash
             anime[3] = anime[3].split("/")
+            arreglo=set(anime[3])
+            anime[3]=list(arreglo)
+            
+            i = (animes(*anime))
+            diccionario1[i.nombre]=(int(i.rating),str(i.estudio),i.genero)
+            
 
-    return
+    return(diccionario1)
+cargar_animes("animes.csv")
+print(diccionario1["Berserk"])
 
 
 def cargar_consultas(path):
+    cola = deque()
     # Abrimos el archivo de animes
     with open(path, 'r', encoding="utf-8") as file:
         # Leemos las lineas
         for line in file.readlines():
             # Los separamos por coma
             consulta = line.strip().split(";")
+            argumentos=consulta[1].split("/")
+            tupla=(consulta[0],argumentos)
+            cola.append(tupla)
+    
+        
 
-    return
+    return cola
+print(cargar_consultas("consultas.csv"))
+

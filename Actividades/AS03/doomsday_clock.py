edeeae3bd81eb08037a6db65e12abf925010a1a8
catalinamusalem <catalina.musalem@uc.cl>
2020-05-07 15:59:58 -0400

    Entrega 1

---
+++ Actividades/AS03/doomsday_clock.py
@@ -0,0 +1,67 @@
+import os
+from threading import Thread
+from time import sleep
+
+from cargar_tweets import cargar_tweets
+from parametros import BOMBA_NUCLEAR, TIEMPO_INICIAL, VELOCIDAD_INICIAL
+
+
+class DoomsdayClock(Thread):
+
+    def __init__(self, velocidad, tiempo_restante):
+        super().__init__()
+
+
+        # No modificar siguientes líneas
+        self.velocidad = velocidad
+        self._tiempo_restante = tiempo_restante
+        self.quedan_lideres = True
+
+    @property
+    def tiempo_restante(self):
+        return self._tiempo_restante
+
+    @tiempo_restante.setter
+    def tiempo_restante(self, value):
+        if value < 0:
+            value = 0
+        self._tiempo_restante = value
+
+    def contar(self):
+        self.tiempo_restante -= 1
+        if not self.tiempo_restante:
+            print("12:00")
+        elif (self.tiempo_restante % 5 == 0 or self.tiempo_restante < 5):
+            print(f"11:{60 - self.tiempo_restante}")
+
+    def run(self):
+        # Completar o modificar si es necesario
+        while self.tiempo_restante > 0 and self.quedan_lideres == True:
+            sleep(1/self.velocidad)
+            self.contar()
+
+
+
+        # No modificar siguientes líneas
+        if self.tiempo_restante == 0:
+            print(BOMBA_NUCLEAR)
+
+    def acelerar(self, nombre, enojo):
+        # Completar o modificar si es necesario
+        self.velocidad += enojo/10
+        print(f"{nombre} ha acelerado el reloj en {enojo/10}")
+
+
+if __name__ == "__main__":
+    # Creamos una instancia de prueba
+    reloj_de_prueba = DoomsdayClock(VELOCIDAD_INICIAL, TIEMPO_INICIAL)
+    tweets_trumpzini = cargar_tweets(os.path.join("datos", "trumpzini_tweets.csv"))
+    # Puedes empezar el thread de reloj_de_prueba aqui:
+    reloj_de_prueba.start()
+    # Con esto podremos ver si tu reloj se acelera y si esta funcionando bien
+    for tweet in tweets_trumpzini:
+        if reloj_de_prueba.tiempo_restante > 1:
+            reloj_de_prueba.acelerar("Persona de prueba", tweet.enojo)
+            print(f"Velocidad actual: {reloj_de_prueba.velocidad}")
+            print(f"Minutos hasta la medianoche: {reloj_de_prueba.tiempo_restante}")
+            sleep(1)
---
+++ Actividades/AS03/lideres.py
@@ -0,0 +1,108 @@
+import os
+import random
+import types
+from threading import Thread, Lock
+from time import sleep
+
+from cargar_tweets import cargar_tweets
+from parametros import ENOJO_INICIAL, PROBABILIDAD_DESAPARECER, PROBABILIDAD_HACKEO
+
+
+class LiderMundial(Thread):
+    lock_global = Lock()
+    def __init__(self, nombre, tweets, enojo, reloj):
+        # Completar
+        
+        super().__init__()
+        self.daemon = True
+        # No modificar
+        self.nombre = nombre
+        self.tweets = tweets
+        self._enojo = int(enojo)
+        self.reloj = reloj
+        self.puede_twitear = True
+        random.shuffle(self.tweets)
+
+    @property
+    def enojo(self):
+        return self._enojo
+
+    @enojo.setter
+    def enojo(self, value):
+        if value < 0:
+            value = 0
+        self._enojo = value
+
+    def run(self):
+        # Completar o modificar si es necesario
+        while self.puede_twitear == True:
+            tiempo = int(max(5*((1.05)**(-self.enojo)),0.25))
+            self.twitear()
+            sleep(tiempo)
+            
+
+    def twitear(self):
+        self.lock_global.acquire()
+        # Completar o modificar si es necesario
+        tweet = random.choice(self.tweets)
+        print(f"{self.nombre}: {tweet.texto}")
+        self.enojo += int(tweet.enojo)
+        self.reloj.acelerar(self.nombre, tweet.enojo)
+        self.lock_global.release()
+
+
+
+class Hacker(LiderMundial, Thread):
+
+    def __init__(self, nombre, trumpzini, dr_pinto, reloj):
+        # No modificar
+        Thread.__init__(self)
+        self.daemon = True
+        self.nombre = nombre
+        self.trumpzini = trumpzini
+        self.dr_pinto = dr_pinto
+        self.reloj = reloj
+
+    def run(self):
+        while self.reloj.quedan_lideres:
+            sleep(0.5)
+            # Completar
+
+
+            # No modificar
+            if not self.trumpzini.puede_twitear and not self.dr_pinto.puede_twitear:
+                self.reloj.quedan_lideres = False
+
+        print("Se ha detenido el Doomsday Clock")
+
+
+if __name__ == "__main__":
+    # Instanciamos al lider mundial
+    tweets_pinto = cargar_tweets(os.path.join("datos", "pin_tweets.csv"))
+    lider_de_prueba = LiderMundial("Dr. Pin Tong-Un", tweets_pinto, ENOJO_INICIAL, None)
+
+    # Ahora necesitamos un reloj que maneje al lider mundial
+    # Necesitamos una función que corra al empezar el thread
+    def probar_reloj(lider):
+        while lider.enojo < 50:
+            sleep(0.1)
+        print(f"{lider.nombre} se enojó tanto que se rompió el reloj :(.")
+
+    # Se crea un thread que representará al reloj
+    reloj_de_prueba = Thread(target=probar_reloj, args=(lider_de_prueba,))
+
+    # Además, el reloj necesita un método acelerar para funcionar dentro de LiderMundial
+    acelerar = lambda reloj, nombre, enojo: print(f"{nombre} ha acelerado el reloj por {enojo / 10}.")
+    reloj_de_prueba.acelerar = types.MethodType(acelerar, reloj_de_prueba)
+
+    # Le pasamos el reloj creado al lider mundial
+    lider_de_prueba.reloj = reloj_de_prueba
+
+    # Empezamos los threads!
+    print("Iniciando prueba...")
+    reloj_de_prueba.start()
+    lider_de_prueba.start()
+    reloj_de_prueba.join()
+
+    if not lider_de_prueba.daemon:
+        print(f"{lider_de_prueba.nombre} está twitteando sin control!")
